f33949f2d1718fa75bbfead0cc47c0f51f8dec38
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    Co-authored-by: althafvly <althafvly@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: If9c55253c8e8786b4998632241a872104b7e7e8e

--- packages/SystemUI/src/com/android/systemui/battery/AccessorizedBatteryDrawable.kt
+++ packages/SystemUI/src/com/android/systemui/battery/AccessorizedBatteryDrawable.kt
@@ -212,6 +212,11 @@ class AccessorizedBatteryDrawable(private val context: Context, frameColor: Int)
         mainBatteryDrawable.setColors(fgColor, bgColor, singleToneColor)
     }
 
+    /** Shows the battery percentage. */
+    fun showPercent(percentage: Boolean) {
+        mainBatteryDrawable.showPercent = percentage
+    }
+
     /** Notifies this drawable that the density might have changed. */
     fun notifyDensityChanged() {
         density = context.resources.displayMetrics.density
